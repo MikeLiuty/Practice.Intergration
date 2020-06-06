be828afdfca77e235dce029e629485ae0fa9cb24
Mike Liu <mikeliuty@gmail.com>
2020-06-06 20:25:25 +1200

    The first intergration test for the default C# web app, which checking the success status code and page type.

---
+++ IntergrationTest/BasicTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Practice.Intergration.SUT;
+using System.Globalization;
+
+namespace IntergrationTest
+{
+    public class BasicTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTest (WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+        
+        [Theory]
+        [InlineData("/")]
+
+        public async Task GetHttpRequest(string url)
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+
+            //Act
+            var response = await client.GetAsync(url);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
